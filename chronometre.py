7a9c5dae6feb12af515890e21171ae301a7f0945
Gerald Pellegrino <geraldpellegrino061212@gmail.com>
2022-11-18 11:31:08 +0100

    working chronometre in command line

---
+++ chronometre.py
@@ -0,0 +1,53 @@
+#coding:utf-8
+import time
+from pynput import keyboard
+
+
+class Chronometre:
+
+    def __init__(self):
+        self.timenow = 0
+        self.state = 0
+
+    def launch(self):
+        self.timenow = time.time()
+        print("timer start")
+    
+    def stop(self):
+        print("time stop")
+        the_time = round(time.time() - self.timenow, 3)
+        print("votre temps : " + str(the_time))  
+        return the_time
+        
+    def space_bar_pressed(self, key):
+        if key == keyboard.Key.space:
+            if self.state == 2:
+                self.stop()
+                self.state = 0
+            elif self.state == 0:
+                self.state = 1
+    
+    def space_bar_released(self, key):
+        if key == keyboard.Key.space:
+            if self.state == 1:
+                self.launch()
+                self.state = 2
+        elif key == keyboard.Key.esc:
+            print("fin du chrono")
+            return False
+
+
+    def listen(self):
+        with keyboard.Listener(
+                on_press=self.space_bar_pressed,
+                on_release=self.space_bar_released) as listener:
+            listener.join()    
+
+    
+def main():
+    chrono = Chronometre()
+    chrono.listen() 
+
+
+if __name__ == "__main__":
+    main()
