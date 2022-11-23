6d3569926274cb438310ccedd93743b7993a1f81
Gerald Pellegrino <geraldpellegrino061212@gmail.com>
2022-11-23 18:23:31 +0100

    test

--- session_manager.py
+++ session_manager.py
@@ -7,16 +7,25 @@ def get_json_file_content(path):
     return result
 
 def save_time_in_session(sessionName, time):
-    session_file: list = []
+    session_file = {}
     if time is not None:
         session_file = get_json_file_content("sessions/{}.json".format(sessionName))
-        session_file.append(time)
+        session_file["times"].append(time)
+        session_file["times_in_session"] += 1
+    with open("sessions/{}.json".format(sessionName), "w", encoding="utf-8") as jsfile:
+        json.dump(session_file, jsfile, indent=2)
+
+def delete_last_time_in_session(sessionName):
+    session_file = {}
+    session_file = get_json_file_content("sessions/{}.json".format(sessionName))
+    session_file["times"]: list .pop(len(session_file["times"])-1)
+    session_file["times_in_session"] -= 1
     with open("sessions/{}.json".format(sessionName), "w", encoding="utf-8") as jsfile:
         json.dump(session_file, jsfile, indent=2)
 
 def create_session(sessionName):
     with open("sessions/{}.json".format(sessionName), "w") as file:
-        json.dump([], file)
+        json.dump({"times_in_session": 0, "times": []}, file)
     session_list = []
     with open("memory.txt", "r", encoding="utf-8") as file:
         session_list = file.read().splitlines()
